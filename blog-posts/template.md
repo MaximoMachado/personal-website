2d96bff1648c194a37fa069f57362816edfbeb78
Maximo Machado <maximomachado246@gmail.com>
2021-07-25 17:20:03 -0400

    Add placeholder files

---
+++ blog-posts/template.md
@@ -0,0 +1,6 @@
+---
+title: 'Template Blog Post'
+date: '2021-07-25'
+---
+
+This is a template blog post so that I can commit the folder to git.
---
+++ components/Layout.ts
@@ -0,0 +1,7 @@
+
+export default function Layout() {
+    // This is a placeholder so that the components file can be added to git
+    return (
+        <div />
+    )
+}
