bc976c870d7ce09c3314b08d12f356205e742c1b
Maximo Machado <maximomachado246@gmail.com>
2021-08-07 00:20:16 -0400

    Utilize chakra components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Box, Center } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Maximo Machado</title>
         <meta name="description" content="A website featuring software developer and photographer Maximo Machado." />
@@ -11,22 +12,24 @@ export default function Home() {
       </Head>
 
       <main>
-        <Image
-          src="/images/profile.png"
-          alt="Picture of Maximo Machado"
-          width={500}
-          height={500}
-        />
-        <div>
-          <h1>
-            Maximo Machado '23
-          </h1>
-          <p>
-            Hello! I'm currently majoring in Computer Science at MIT.
-            Software development and programming is something I've loved since I was a kid, budding from my interests in playing and creating video games.
-          </p>
-        </div>
+        <Center >
+          <Image
+            src="/images/profile.png"
+            alt="Picture of Maximo Machado"
+            width={100}
+            height={100}
+          />
+          <Box >
+            <h1>
+              Maximo Machado '23
+            </h1>
+            <p>
+              Hello! I'm currently majoring in Computer Science at MIT.
+              Software development and programming is something I've loved since I was a kid, budding from my interests in playing and creating video games.
+            </p>
+          </Box >
+        </Center >
       </main>
-    </div>
+    </>
   )
 }
